3ede2a06e99760f313b15579087c82c161822e62
Felicity Rhone <frhone@latitudegeo.com>
2024-07-16 10:49:16 -0700

    Install latest community toolkit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace SnackbarSample
 {
@@ -9,6 +10,7 @@ namespace SnackbarSample
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
