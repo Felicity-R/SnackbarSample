5b9a2617cb4718b7aa030a61702e560f3fabc63c
Felicity Rhone <frhone@latitudegeo.com>
2024-07-15 17:01:49 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SnackbarSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
